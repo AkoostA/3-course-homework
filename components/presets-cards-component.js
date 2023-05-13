6e90260bbdf911b77adacf18ae06f717de4b9b49
AkoostA <123000618+AkoostA@users.noreply.github.com>
2023-05-13 22:45:19 +0300

    Глобальный объект через window

--- components/presets-cards-component.js
+++ components/presets-cards-component.js
@@ -1,23 +1,21 @@
 import { renderingDifficulty } from "./difficulty-component.js"
-import { mainElement, globalData } from "../index.js"
-
 
 function renderingPresetsCards() {
-    switch (globalData.difficulty) {
+    switch (window.globalThis.difficulty) {
         case "easy":
-            mainElement.innerHTML = `
+            window.globalThis.mainElement.innerHTML = `
             <h1>ЗДЕСЬ БУДЕТ ЛЕГКИЙ УРОВЕНЬ СЛОЖНОСТИ</h1>
             <button class="difficulty__buttonStart">Назад</button>
             `
             break;
         case "average":
-            mainElement.innerHTML = `
+            window.globalThis.mainElement.innerHTML = `
             <h1>ЗДЕСЬ БУДЕТ СРЕДНИЙ УРОВЕНЬ СЛОЖНОСТИ</h1>
             <button class="difficulty__buttonStart">Назад</button>
             `
             break;
         case "hard":
-            mainElement.innerHTML = `
+            window.globalThis.mainElement.innerHTML = `
             <h1>ЗДЕСЬ БУДЕТ ВЫСОКИЙ УРОВЕНЬ СЛОЖНОСТИ</h1>
             <button class="difficulty__buttonStart">Назад</button>
             `
--- index.js
+++ index.js
@@ -1,51 +1,9 @@
 import { renderingDifficulty } from "./components/difficulty-component.js";
-import { renderingPresetsCards } from "./components/presets-cards-component.js";
 
-const mainElement = document.getElementById("main");
-const globalData = {
+globalThis = {
+    mainElement: document.getElementById("main"),
     difficulty: "",
     cards: "",
 }
 
 renderingDifficulty();
-
-function checkButtonDifficulty() {
-    const chooseButtonElements = document.querySelectorAll(".difficulty__chooseButton");
-    const startButtonElement = document.querySelector(".difficulty__buttonStart");
-
-    for (const chooseButtonElement of chooseButtonElements) {
-        chooseButtonElement.addEventListener("click", () => {
-            for (const chooseButtonElement of chooseButtonElements) {
-                chooseButtonElement.classList.remove("-button-active")
-            }
-            globalData.difficulty = chooseButtonElement.textContent;
-            chooseButtonElement.classList.add("-button-active")
-            switch (globalData.difficulty) {
-                case "1":
-                    globalData.difficulty = "easy"
-                    globalData.cards = "6 карточек (3 пары)";
-                    break;
-                case "2":
-                    globalData.difficulty = "average"
-                    globalData.cards = "12 карточек (6 пар)";
-                    break;
-                case "3":
-                    globalData.difficulty = "hard"
-                    globalData.cards = "18 карточек (9 пар)";
-                    break;
-                default:
-                    break;
-            }
-        })
-    }
-
-    startButtonElement.addEventListener("click", () => {
-        if (!globalData.difficulty) {
-            alert("С начало выберете сложность");
-            return;
-        }
-        renderingPresetsCards();
-    })
-}
-
-export { mainElement, globalData, checkButtonDifficulty }
